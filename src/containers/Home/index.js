a243d6f65392e737013f6006ca50d7862492fde8
kirish-r <kirirajes81@gmail.com>
2021-05-31 20:41:33 +0530

    project admin panel

---
+++ src/containers/Home/index.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import Layout from '../../components/layout';
+
+
+/**
+* @author
+* @function Home
+**/
+
+const Home = (props) => {
+  return (
+    <Layout sidebar>
+
+  
+    </Layout>
+
+
+  )
+
+}
+
+export default Home
